6a5d62d7db2d3c708e3a5ac50f887910cc0ba42e
Spenser Truex <truex@equwal.com>
2023-06-02 14:30:09 -0300

    CL-WHO plugin working

--- plugins/cl-who.lisp
+++ plugins/cl-who.lisp
@@ -2,17 +2,21 @@
   (ql:quickload 'cl-who))
 
 (defpackage :coleslaw-cl-who
-  (:use #:cl)
+  (:use #:cl #:cl-who)
   (:export #:enable))
 
 (in-package :coleslaw-cl-who)
 
 (defmethod coleslaw:render-text (text (format (eql :cl-who)))
-  (with-input-from-string (s (concatenate 'string
-                                          #.(format nil "(cl-who:with-html-output-to-string ()~%")
-                                          text
-                                          #.(format nil "~%)")))
-      (read s)))
-
-(defun enable ()
+  (with-output-to-string (o)
+      (with-input-from-string (s (prog2 (in-package :cl-who);kludge
+                                        (concatenate 'string
+                                              (format nil "(cl-who:with-html-output-to-string (o) (htm~%")
+                                              text
+                                              (format nil "~%))"))
+                                        (in-package :coleslaw-cl-who)))
+        (format o "~a" (eval (read s nil)))))
+  (in-package :coleslaw);bugfix for kludge
   )
+
+(defun enable ())
